2b7014b568d5540e196729311cc54790988cd560
Benjamin Marconnet <benjamin.marconnet@outlook.com>
2021-01-31 23:48:30 +0100

    add division pie chart

---
+++ MarconnetDotFr.Core/Models/FootyStats/DivisionCountModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarconnetDotFr.Core.Models.FootyStats
+{
+    public class DivisionCountModel
+    {
+        public Division division { get; set; }
+
+        public int count { get; set; }
+
+        public string strDivision => division == Division.D1 ? "D1" : "D2";
+    }
+}
--- MarconnetDotFr/Pages/FootyStats.cshtml.cs
+++ MarconnetDotFr/Pages/FootyStats.cshtml.cs
@@ -17,6 +17,10 @@ namespace MarconnetDotFr.Pages
         private readonly IFootyRepository _footyRepository;
         private IList<SeasonModel> seasons;
 
+        public int NbLeagueTitleWon { get; set; }
+        public int NbCupWon { get; set; }
+        public int NbEuropeanTitleWon { get; set; }
+
         public FootyStatsModel(IConfiguration configuration, IFootyRepository footyRepository)
         {
             _configuration = configuration;
@@ -25,7 +29,14 @@ namespace MarconnetDotFr.Pages
 
         public void OnGet()
         {
-            // nothing special to do
+            if (seasons == null)
+            {
+                seasons = _footyRepository.GetSeasons("fcsm").Reverse().ToList();
+            }
+
+            NbLeagueTitleWon = seasons.Where(x => x.ranking == 1).Count();
+            NbCupWon = seasons.Where(x => x.coupedefrance == CupPerformance.Winner).Count() + seasons.Where(x => x.coupedelaligue == CupPerformance.Winner).Count();
+            NbEuropeanTitleWon = seasons.Where(x => x.europe == "Vainqueur").Count();
         }
 
         public JsonResult OnPostRead()
@@ -39,6 +50,20 @@ namespace MarconnetDotFr.Pages
             seasons.Single(x => x.season == "2001/2002").notes = "Nouveau Bonal";
 
             return new JsonResult(seasons);
+        }
+
+        public JsonResult OnPostReadDivision()
+        {
+            if (seasons == null)
+            {
+                seasons = _footyRepository.GetSeasons("fcsm").Reverse().ToList();
+            }
+
+            IList<DivisionCountModel> divisionCount = new List<DivisionCountModel>();
+            divisionCount.Add(item: new DivisionCountModel() { division = Division.D1, count = seasons.Where(x => x.division == Division.D1).Count() });
+            divisionCount.Add(item: new DivisionCountModel() { division = Division.D2, count = seasons.Where(x => x.division == Division.D2).Count() });
+
+            return new JsonResult(divisionCount);
         }
     }
 }
