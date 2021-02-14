76d2f548806b3d75086cb7411939ac9fc5ce1abd
Benjamin Marconnet <benjamin.marconnet@outlook.com>
2021-02-14 16:09:01 +0100

    [reorga] map on homepage

---
+++ MarconnetDotFr/wwwroot/js/index/main.js
@@ -0,0 +1,43 @@
+﻿var App = (function () {
+
+    function init() {
+        MapHelper.init(DOMFinder.getMapDiv().id);
+        MapHelper.setView();
+        setMarkers();
+    }
+
+    function setMarkers() {
+        // remove all markers on the map
+        MapHelper.cleanMarkers();
+
+        // Belfort
+        let belfort = new Coordinate(47.6379599, 6.8628942);
+        MapHelper.addMarker(belfort, true);
+
+        // Portsmouth
+        let portsmouth = new Coordinate(50.8036831, -1.075614);
+        MapHelper.addMarker(portsmouth, true);
+
+        // Basel
+        let basel = new Coordinate(47.5581077, 7.5878261);
+        MapHelper.addMarker(basel, true);
+
+        // Iselin
+        let iselin = new Coordinate(40.569334999999995, -74.31511249016258);
+        MapHelper.addMarker(iselin, true);
+
+        // Quebec
+        let quebec = new Coordinate(46.8259601, -71.2352226);
+        MapHelper.addMarker(quebec, true);
+
+        // Geneva
+        let geneva = new Coordinate(46.2017559, 6.1466014);
+        MapHelper.addMarker(geneva, true);
+    }
+
+    return {
+        init: init
+    };
+})();
+
+App.init();
---
+++ MarconnetDotFr/wwwroot/js/tools.js
@@ -0,0 +1,57 @@
+﻿var Tools = (function () {
+    function isInteger(element) {
+        return /^-?[0-9]+$/.test(element);
+    }
+
+    return {
+        isInteger: isInteger
+    };
+})();
+
+var LazyLoadingTools = (function () {
+
+    let scriptsLoaded = [];
+    let competitionsCount;
+    let onCompetitionScriptsLoadedCallback;
+
+    function loadScript(url) {
+
+        if (scriptsLoaded.indexOf(url) > -1) {
+            scriptLoaded();
+            return;
+        }
+
+        const head = document.getElementsByTagName('head')[0];
+        let script = document.createElement("script");
+        script.src = url;
+        script.onload = scriptLoaded;
+        script.onreadystatechange = function () {
+            if (this.readyState == 'complete') {
+                scriptLoaded();
+            }
+        };
+
+        head.appendChild(script);
+        scriptsLoaded.push(url);
+    }
+
+    function scriptLoaded() {
+        competitionsCount--;
+        if (competitionsCount === 0)
+            onCompetitionScriptsLoadedCallback();
+    }
+
+    function setCompetitionsCount(count) {
+        competitionsCount = count;
+    }
+
+    function setOnCompetitionScriptsLoadedCallback(callback) {
+        onCompetitionScriptsLoadedCallback = callback;
+    }
+
+    return {
+        loadScript: loadScript,
+        setCompetitionsCount: setCompetitionsCount,
+        setOnCompetitionScriptsLoadedCallback: setOnCompetitionScriptsLoadedCallback
+    };
+})();
