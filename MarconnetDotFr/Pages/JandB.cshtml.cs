01079ea3f138135d553a12f6dac357dd89789f80
Benjamin Marconnet <benjamin.marconnet@outlook.com>
2021-12-22 12:40:57 +0100

    add jandb

---
+++ MarconnetDotFr/Pages/JandB.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MarconnetDotFr.Pages
+{
+    public class JandBModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
